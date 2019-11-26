c2d3751ca9e710c30c544e5102c51d840759f351
levicosta201 <levi.costa1@gmail.com>
2019-11-25 22:23:12 -0300

    fix comment

--- src/Main.java
+++ src/Main.java
@@ -90,6 +90,12 @@ public class Main {
         }, 0, SECONDS * 5);
     }
 
+    /**
+     * Generate a random Integer from min and max range
+     * @param min
+     * @param max
+     * @return
+     */
     public static Integer getRandomIntegerBetweenRange(Integer min, Integer max){
         Integer rand_value = (int)(Math.random() * ((max-min) + 1)) + min;
         return Integer.valueOf(rand_value);
